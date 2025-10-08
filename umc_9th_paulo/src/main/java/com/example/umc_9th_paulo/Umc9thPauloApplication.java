fa67c07cf523594d79df5ca374080588e74dd533
keypang <bestnest820@naver.com>
2025-10-09 02:33:44 +0900

    feat: BaseEntity 추가

---
+++ umc_9th_paulo/src/main/java/com/example/umc_9th_paulo/Umc9thPauloApplication.java
@@ -0,0 +1,13 @@
+package com.example.umc_9th_paulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc9thPauloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Umc9thPauloApplication.class, args);
+    }
+
+}
---
+++ umc_9th_paulo/src/test/java/com/example/umc_9th_paulo/Umc9thPauloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umc_9th_paulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc9thPauloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
