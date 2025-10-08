29ff09b070ccfef74f625b5cdd1ac31b6caecd76
keypang <bestnest820@naver.com>
2025-10-09 03:02:47 +0900

    feat: userauth entity 생성

---
+++ umc_9th_paulo/src/main/java/com/example/umc_9th_paulo/domain/user/entity/UserAuth.java
@@ -0,0 +1,23 @@
+package com.example.umc_9th_paulo.domain.user.entity;
+
+import com.example.umc_9th_paulo.domain.user.enums.LoginType;
+import com.example.umc_9th_paulo.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "user_auths")
+public class UserAuth extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "login_type", nullable = false)
+    private LoginType loginType;
+}
