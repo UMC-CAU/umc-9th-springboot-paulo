ad03340a7d7f0738f1a8748f3f7d2e86860ea7f8
keypang <bestnest820@naver.com>
2025-10-09 03:24:37 +0900

    feat: region entity 생성

---
+++ umc_9th_paulo/src/main/java/com/example/umc_9th_paulo/domain/restaurant/entity/Region.java
@@ -0,0 +1,27 @@
+package com.example.umc_9th_paulo.domain.restaurant.entity;
+
+import com.example.umc_9th_paulo.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "region")
+public class Region extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "latitude", nullable = false)
+    private Double latitude;
+
+    @Column(name = "longitude", nullable = false)
+    private Double longitude;
+}
