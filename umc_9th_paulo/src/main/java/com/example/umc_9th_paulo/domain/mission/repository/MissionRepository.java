c247aafb88a182f1dec131499cc38f3d43b35d28
keypang <bestnest820@naver.com>
2025-11-04 14:13:38 +0900

    feat: 아마 시니어 미션

--- umc_9th_paulo/src/main/java/com/example/umc_9th_paulo/domain/mission/repository/MissionRepository.java
+++ umc_9th_paulo/src/main/java/com/example/umc_9th_paulo/domain/mission/repository/MissionRepository.java
@@ -3,16 +3,19 @@ package com.example.umc_9th_paulo.domain.mission.repository;
 import com.example.umc_9th_paulo.domain.mission.entity.Mission;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
-
-    @Query("select m from Mission m " +
-            "join Restaurant rt on m.restaurant.id = rt.id " +
-            "join Region ri on rt.region.id = ri.id " +
-            "where ri.id =:regionId and m.remainingDuration > 0 and " +
-            "m.id not in (select um.mission.id from UserMission um where um.user.id =:userId)")
+        @EntityGraph(attributePaths = {"restaurant"})
+        @Query("select m from Mission m " +
+                "where m.restaurant.region.id = :regionId " +
+                "and m.remainingDuration > 0 " +
+                "and not exists (" +
+                "  select 1 from UserMission um " +
+                "  where um.user.id = :userId and um.mission.id = m.id" +
+                ")")
     Page<Mission> findByRegionAndUserNotIn(@Param("regionId") Long regionId, @Param("userId") Long userId, Pageable pageable);
 }
